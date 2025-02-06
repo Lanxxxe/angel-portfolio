54e9d34fcce6b064be49d2dd0be7d7554a94846c
Lanxxxe <Lanxxxe>
2025-02-06 13:19:10 +0800

    modify: modal contents modify: responsiveness modify: some designs

--- js/contact.js
+++ js/contact.js
@@ -1,29 +1,29 @@
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    let isValid = true;
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    
-    document.getElementById("nameError").textContent = "";
-    document.getElementById("emailError").textContent = "";
-    
-    if (name.trim() === "") {
-        document.getElementById("nameError").textContent = "Name is required.";
-        isValid = false;
-    }
-    if (!emailPattern.test(email)) {
-        document.getElementById("emailError").textContent = "Enter a valid email address.";
-        isValid = false;
-    }
-    
-    if (isValid) {
-        alert("Message Sent Successfully!");
-        document.getElementById("contactForm").reset();
-    }
-});
-
 document.addEventListener('DOMContentLoaded', () => {
     const currentPage = document.querySelector("#contact");
     currentPage.classList.add('active');
+
+    document.getElementById("contactForm").addEventListener("submit", function(event) {
+        event.preventDefault();
+        let isValid = true;
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        
+        document.getElementById("nameError").textContent = "";
+        document.getElementById("emailError").textContent = "";
+        
+        if (name.trim() === "") {
+            document.getElementById("nameError").textContent = "Name is required.";
+            isValid = false;
+        }
+        if (!emailPattern.test(email)) {
+            document.getElementById("emailError").textContent = "Enter a valid email address.";
+            isValid = false;
+        }
+        
+        if (isValid) {
+            alert("Message Sent Successfully!");
+            document.getElementById("contactForm").reset();
+        }
+    });
 })
--- js/projects.js
+++ js/projects.js
@@ -1,12 +1,13 @@
+
+function openModal(projectId) {
+    document.getElementById(projectId).style.display = 'flex';
+}
+function closeModal(projectId) {
+    document.getElementById(projectId).style.display = 'none';
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const currentPage = document.querySelector("#projects");
     currentPage.classList.add('active');
-
-    function openModal(projectId) {
-        document.getElementById(projectId).style.display = 'flex';
-    }
-    function closeModal(projectId) {
-        document.getElementById(projectId).style.display = 'none';
-    }
 
 })
